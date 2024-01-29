09c94b9c4b3c97b3650d62f15f3439ad4e97457d
Geossephy Jumbo <geossephyjumboalava@gmail.com>
2024-01-29 00:56:12 -0500

    Update useKeyboardEvent.js

--- src/hooks/useKeyboardEvent.js
+++ src/hooks/useKeyboardEvent.js
@@ -4,7 +4,6 @@ export function useKeyboardEvent ({ setOpen }) {
   useEffect(() => {
     document.addEventListener('keydown', (event) => {
       const keyValue = event.key
-      // const codeValue = event.code
       if (keyValue.match('Escape')) setOpen(false)
     }, false)
   }, [])
