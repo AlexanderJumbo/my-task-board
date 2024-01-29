5c4b33a9eb77223a97f1941f074135f126d1ffc9
Geossephy Jumbo <geossephyjumboalava@gmail.com>
2024-01-29 00:57:58 -0500

    update useTask.js

--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react'
 import { db } from '../database/firebaseConfig'
 
 export function useTask ({ task }) {
-  console.log('🚀 ~ useTask ~ task:', task)
   const [taskFetch, setTaskFetch] = useState({})
   const [status, setStatus] = useState('')
 
@@ -11,7 +10,6 @@ export function useTask ({ task }) {
     const document = doc(db, 'tasks', task)
     const result = await getDoc(document)
     if (result.exists()) {
-      console.log(result.data())
       setTaskFetch(result.data())
       setStatus(result?.data()?.status)
     } else {
